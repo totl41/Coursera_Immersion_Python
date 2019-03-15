47da0b7a38672f3b50e19f02950f2317cc5ada82
totl41 <naumovbox@gmail.com>
2019-03-15 21:25:42 +0300

    Добавлена проверка аргумента (строки) на наличие в нем, только цифр

--- hello/SumDigits.py
+++ hello/SumDigits.py
@@ -7,9 +7,14 @@ print(f"int args: '{sys.argv}'")
 if len(sys.argv) < 2:
     sys.exit('Введена пустая строка')
 
+if not sys.argv[1].isdigit():
+    sys.exit("Строка должна состоять, только из цифр")
+
 sum_digits: int = 0
 
 for list_item in sys.argv[1]:
     sum_digits = sum_digits + int(list_item)
 
 print(sum_digits)
+
+
